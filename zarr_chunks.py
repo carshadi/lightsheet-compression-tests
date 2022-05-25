66474c93c1364af80cac45dc33fbb8cd9ce6c6ea
Cameron Arshadi <arshadic23@gmail.com>
2022-05-25 13:02:02 -0700

    fix zarr_chunks.py

--- zarr_chunks.py
+++ zarr_chunks.py
@@ -13,6 +13,7 @@ import argparse
 import pandas as pd
 import logging
 import time
+import datetime
 
 if __name__ == "__main__":
 
@@ -41,40 +42,42 @@ if __name__ == "__main__":
     logging.info(f"using {threads} threads")
 
     metrics_fpath = args.metrics_file
-    
-    #compressor = numcodecs.Blosc(cname='zstd', clevel=1, shuffle=numcodecs.Blosc.SHUFFLE)
-    compressor = None
-    
+
+    compressor = numcodecs.Blosc(cname='zstd', clevel=1, shuffle=numcodecs.Blosc.SHUFFLE)
+    # compressor = None
+
     bits = 2
     scale = 1.0 / (2 ** bits)
-    #filters = [ numcodecs.fixedscaleoffset.FixedScaleOffset(offset=0, scale=scale, dtype=np.uint16) ]
+    # filters = [ numcodecs.fixedscaleoffset.FixedScaleOffset(offset=0, scale=scale, dtype=np.uint16) ]
     filters = []
-    
-    chunk_factors = [1, 2, 4, 8, 16, 32, 64, 128]
-    
+
+    chunk_factors = [2]
+
+    print(f"starting tests at {datetime.datetime.now()}")
+
     all_metrics = []
     for cf in chunk_factors:
-        chunks_metrics = {}
         # chunk along Z
         chunks = (int(math.ceil(data.shape[0] / (threads * cf))), data.shape[1], data.shape[2])
-        chunks_metrics['chunk_shape'] = chunks
         logging.info(f"chunk shape: {chunks}")
 
         chunk_size = np.product(chunks) * data.itemsize
-        chunks_metrics['chunk_size'] = chunk_size
-        logging.info(f"chunk size: {chunk_size}")
+        logging.info(f"chunk size: {chunk_size / 2 ** 20} MiB")
 
         blocks = np.array(chunks)
         # block shape (thread FOV) must be a multiple of chunk shape
         blocks[0] *= cf
-        chunks_metrics['block_shape'] = blocks
         logging.info(f"block shape: {blocks}")
 
-        blocklist = zarr_parallel_test.make_blocks(data.shape, blocks)
+        blocklist = zarr_io_test.make_blocks(data.shape, blocks)
         logging.info(f"num blocks: {len(blocklist)}")
 
         num_trials = 5
         for i in range(num_trials):
+            chunks_metrics = {}
+            chunks_metrics['chunk_shape'] = chunks
+            chunks_metrics['chunk_size'] = chunk_size
+            chunks_metrics['block_shape'] = blocks
             # Write the output Zarr
             start = timer()
             zarr_io_test.write_threading(
@@ -92,7 +95,7 @@ if __name__ == "__main__":
             write_speed = data.nbytes / write_time
             chunks_metrics['write_time'] = write_time
             chunks_metrics['write_bps'] = write_speed
-            logging.info(f"write time: {write_time}, compress MiB/s {write_speed / 2**20}")
+            logging.info(f"write time: {write_time}, compress MiB/s {write_speed / 2 ** 20}")
 
             # Read the output Zarr
             start = timer()
@@ -108,3 +111,5 @@ if __name__ == "__main__":
 
     df = pd.DataFrame.from_records(all_metrics)
     df.to_csv(metrics_fpath, index_label='test_number')
+
+    print(f"finished tests at {datetime.datetime.now()}")
